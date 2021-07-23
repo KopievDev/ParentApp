cf0764545fbe1cd3b8f6d300b905d0925d1a84ae
Ivan Kopiev <kopiev@mustdev.ru>
2021-07-23 15:16:01 +0300

    Initial Commit

---
+++ ParentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ParentApp
+//
+//  Created by Ivan Kopiev on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
